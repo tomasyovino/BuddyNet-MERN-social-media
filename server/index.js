e933b9a0ec74373dad06db365dada7b26d7cedf4
tomasyovino <tomasyovino@gmail.com>
2023-02-07 15:15:19 -0300

    Add routes initial structure

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import router from "./src/routes/index.js";
 import config from "./src/utils/config.js";
 import { DBConnect } from "./src/utils/db.js";
 
@@ -22,17 +22,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
-/* FILE STORAGE */
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname)
-    }
-});
-const upload = multer({ storage });
+app.use("/api", router);
 
 /* SERVER CONNECTION */
 try {
