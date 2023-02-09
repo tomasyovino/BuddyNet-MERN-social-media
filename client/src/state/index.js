238a6fc2ceae1789d66ece8136171636e16b798e
tomasyovino <tomasyovino@gmail.com>
2023-02-09 10:13:01 -0300

    Add redux setup

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,5 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    
-}
+    mode: "light",
+    user: null,
+    token: null,
+    posts: []
+};
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducer: {
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+        setLogin: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
+        },
+        setFriends: (state, action) => {
+            if(state.user) {
+                state.user.friends = action.payload.friends;
+            } else {
+                console.log("No user is logged in");
+            };
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+        },
+        setPost: (state, action) => {
+            const updatedPost = state.posts.map((post) => post._id === action.payload.post._id ? action.payload.post : post);
+            state.posts = updatedPost;
+        }
+    }
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
+export default authSlice.reducer;
