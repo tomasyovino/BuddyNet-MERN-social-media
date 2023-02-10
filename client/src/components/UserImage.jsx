b3486d366fac617675dc6ddb610a9c4e7154d728
tomasyovino <tomasyovino@gmail.com>
2023-02-10 14:18:01 -0300

    Add user widgets & start Home Page design

---
+++ client/src/components/UserImage.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+
+const UserImage = ({ image, size = "60px" }) => {
+    return (
+        <Box width={size} height={size}>
+            <img 
+                style={{ objectFit: "cover", borderRadius: "50%" }}
+                width={size}
+                height={size}
+                src={`${process.env.REACT_APP_BASE_URL}/assets/${image}`}
+                alt="User" 
+            />
+        </Box>
+    );
+};
+
+export default UserImage;
